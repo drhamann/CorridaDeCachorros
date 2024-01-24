154847225bacfb32480d4704fe179008e29ce9a1
George Hamann <george.hamann@gmail.com>
2024-01-23 22:24:55 -0300

    Corrida rodando pela web Flata melhorar estilo e posição dos itens

--- CorridaDeCachorros.Mvc/Controllers/CorridaDeCachorrosController.cs
+++ CorridaDeCachorros.Mvc/Controllers/CorridaDeCachorrosController.cs
@@ -5,7 +5,7 @@ namespace CorridaDeCachorros.Mvc.Controllers
     public class CorridaDeCachorrosController : Controller
     {
         public CorridaDeCachorro CorridaDeCachorro { get; set; }
-
+        public static string SumarioCorrida { get; set; } = string.Empty;
         public CorridaDeCachorrosController(
             CorridaDeCachorro corridaDeCachorro)
         {
@@ -14,7 +14,25 @@ namespace CorridaDeCachorros.Mvc.Controllers
 
         public IActionResult Index()
         {
+            ViewData["SumarioCorrida"] = SumarioCorrida;
             return View(CorridaDeCachorro);
+        }
+
+        [HttpPost]
+        public IActionResult Aposta(
+            [Bind("nomeCorredor,nomeApostador,valorAposta")]
+            string nomeCorredor, string nomeApostador, double valorAposta)
+        {
+            // Use the idDoCorredor parameter in your logic
+            CorridaDeCachorro.Apostar(nomeApostador,nomeCorredor,valorAposta);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CorrerAsync()
+        {
+            SumarioCorrida = await CorridaDeCachorro.Correr();
+            return RedirectToAction("Index");
         }
     }
 }
--- CorridaDeCachorros.Mvc/Program.cs
+++ CorridaDeCachorros.Mvc/Program.cs
@@ -21,6 +21,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=CorridaDeCachorros}/{action=Index}/{id?}");
 
 app.Run();
